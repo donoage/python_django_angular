a874cc825f4b177b829ca6d9d617d4c75b56f334
Stephen Bae <donoage@gmail.com>
2017-04-02 17:29:21 -0400

    remove test url

--- zipari/urls.py
+++ zipari/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # API:V1
     url(r'^api/v1/', include(api_router.urls)),
-    url(r'^api/v1/list-insurance', InsuranceList.as_view()),
     # Web App Entry
     url(r'^$', TemplateView.as_view(template_name="app/index.html"), name='index'),
     url(r'^docs/', include_docs_urls(title='My API title'))
